917bb1c5499ad27c1247234008a7935e52293f0b
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-06 10:43:09 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import rocketImage from '../assets/rocket.jpg';
+
+
+const Navbar = () => {
+  const navItems = ['Pricing', 'Leaderboards'];
+
+  return (
+    <nav className="flex items space-x-9 bg-black p-4">
+        <div className='flex'>  
+            <img
+            src={rocketImage}
+            alt ="logo"
+            className='w-8 h-8'
+            />
+            <span className='text xl font-bold text-white'>SuperShip</span>
+        </div>
+      {navItems.map((item, index) => (
+        <a 
+          key={index} 
+          href={`#${item.toLowerCase()}`} 
+          className="text-white hover:text-blue-500 transition-colors"
+        >
+          {item}
+        </a>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
